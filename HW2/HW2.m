4bf29881e9669f9631fb05cc92f273180fc1cd1e
6cyu <syal_6cyu@yahoo.co.id>
2015-02-12 21:52:20 +0100

    revise HW2 1

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -1,4 +1,4 @@
-%%
+%% Part 1
 clear all; close all; clc;
 
 load handel;
@@ -31,18 +31,18 @@ for i=1:length(width)
         vft = fft(vf);
         spec(i,j,:) = abs(fftshift(vft));
         
-        figure(1),
-        subplot(3,1,1),plot(t,v,t,f,'r');
-        figure(1),
-        subplot(3,1,2),plot(t,vf);
-        figure(1),
-        subplot(3,1,3),plot(ks,abs(fftshift(vft))/max(abs(fftshift(vft))));
+%         figure(1),
+%         subplot(3,1,1),plot(t,v,t,f,'r');
+%         figure(1),
+%         subplot(3,1,2),plot(t,vf);
+%         figure(1),
+%         subplot(3,1,3),plot(ks,abs(fftshift(vft))/max(abs(fftshift(vft))));
 %         pause(0.1);  
         
     end
 end
 
-% spectrogram plot
+%% spectrogram plot
 dum(:,:) = spec(1,:,:);
 figure(2), subplot(2,2,1), 
 pcolor(tslide,ks,dum.'), shading interp,
@@ -59,3 +59,27 @@ dum(:,:) = spec(4,:,:);
 figure(2), subplot(2,2,4),
 pcolor(tslide,ks,dum.'), shading interp,
 colormap(hot),colorbar;
+
+%% Part 2 - piano
+[y_piano,Fs_piano] = audioread('music1.wav'); 
+% Fs_piano = length(y_piano)/tr_piano;
+tr_piano = length(y_piano)/Fs_piano;
+n_piano = 
+plot((1:length(y_piano))/Fs_piano,y_piano);
+xlabel('Time [sec]'); ylabel('Amplitude');
+title('Mary had a little lamb (piano)'); drawnow;
+p8_piano = audioplayer(y_piano,Fs_piano); playblocking(p8_piano);
+
+
+
+
+%% Part 2 - recorder
+
+figure(2)
+[y_rec,Fs_rec] = audioread('music2.wav'); 
+% Fs_rec = length(y_rec)/tr_rec;
+tr_rec = length(y_rec)/Fs_rec;
+plot((1:length(y_rec))/Fs_rec,y_rec);
+xlabel('Time [sec]'); ylabel('Amplitude');
+title('Mary had a little lamb (recorder)');  
+p8_rec = audioplayer(y_rec,Fs_rec); playblocking(p8_rec);
